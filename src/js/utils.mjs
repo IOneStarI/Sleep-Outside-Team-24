bacd232ebc62dd35eeda7d4be305f9cf893f7414
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2025-11-08 14:31:00 +0200

    vd--individual12

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,10 +29,10 @@ export function getParam(param) {
   return urlParams.get(param);
 }
 
-// export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
-//   const htmlStrings = list.map(template);
-//   if (clear) {
-//     parentElement.innerHTML = "";
-//   }
-//   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-// }
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
