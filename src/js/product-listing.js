25daa5fbead69ceabc88d65227a2876eb3e40062
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2025-11-15 16:03:42 +0200

    lint fixes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,14 +1,14 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
-import { loadHeaderFooter, getParam  } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam  } from "./utils.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter();
 });
 
-const category = getParam('category');
+const category = getParam("category");
 const dataSource = new ProductData();
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 
 const myList = new ProductList(category, dataSource, listElement);
 
